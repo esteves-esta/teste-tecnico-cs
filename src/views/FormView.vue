1c124f5d090d1f6f89ef6b6c6b7288748e58c101
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2025-01-22 15:41:56 -0300

    feat: removendo estilos padrão e criando base do projeto

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="form">
+    <h1>Novo projeto</h1>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
+  .form {
     display: flex;
     align-items: center;
   }
