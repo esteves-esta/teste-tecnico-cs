110e86159bcb2a1ce2f348157eb8da6b19fede34
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2025-01-24 19:25:39 -0300

    refactor: mudando header para componente próprio

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,11 @@
 <script setup lang="ts">
-import SearchBar from '@/components/SearchBar/SearchBar.vue'
-import Icon from '@/components/Icon/Icon.vue'
-import VisuallyHidden from '@/components/VisuallyHidden/VisuallyHidden.vue'
+import Header from '@/components/Header/Header.vue'
 import BaseView from '@/views/BaseView.vue'
-import { useSearch } from './stores/search'
 import { ToastProvider } from 'radix-vue'
-const searchStore = useSearch()
 </script>
 
 <template>
-  <header :class="classes.header">
-    <SearchBar v-if="searchStore.searchIsVisible" />
-    <template v-else>
-      <img alt="App logo" :class="classes.logo" src="@/assets/logo.svg" />
-      <button :class="classes.search_btn" @click="searchStore.toogleSearch">
-        <Icon name="Search" :size="18" :stroke_width="2">
-          <VisuallyHidden>Pesquisar</VisuallyHidden>
-        </Icon>
-      </button>
-    </template>
-  </header>
+  <Header />
   <main :class="classes.main">
     <ToastProvider>
       <BaseView />
@@ -28,33 +14,6 @@ const searchStore = useSearch()
 </template>
 
 <style module="classes">
-.header {
-  position: relative;
-  color: var(--white);
-  line-height: 1.5;
-  height: 100%;
-  display: flex;
-  background: var(--color-primary-dark);
-  align-items: center;
-  box-shadow: 0px 4px 4px 0px hsl(0 0 0% / 0.25);
-}
-
-.logo {
-  padding-right: 64px;
-  display: block;
-  margin: 0 auto;
-}
-
-.search_btn {
-  cursor: pointer;
-  padding: 10px;
-  border-radius: 100%;
-  &:hover,
-  &:focus {
-    color: var(--color-accent);
-  }
-}
-
 .main {
   margin: 60px 42px;
 }
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import SearchBar from '@/components/SearchBar/SearchBar.vue'
+import Icon from '@/components/Icon/Icon.vue'
+import VisuallyHidden from '@/components/VisuallyHidden/VisuallyHidden.vue'
+import { useSearch } from '@/stores/search'
+const searchStore = useSearch()
+</script>
+
+<template>
+  <header :class="classes.header">
+    <SearchBar v-if="searchStore.searchIsVisible" />
+    <template v-else>
+      <img alt="App logo" :class="classes.logo" src="@/assets/logo.svg" />
+      <button :class="classes.search_btn" @click="searchStore.toogleSearch">
+        <Icon name="Search" :size="18" :stroke_width="2">
+          <VisuallyHidden>Pesquisar</VisuallyHidden>
+        </Icon>
+      </button>
+    </template>
+  </header>
+</template>
+
+<style module="classes">
+.header {
+  position: relative;
+  color: var(--white);
+  line-height: 1.5;
+  height: 100%;
+  display: flex;
+  background: var(--color-primary-dark);
+  align-items: center;
+  box-shadow: 0px 4px 4px 0px hsl(0 0 0% / 0.25);
+}
+
+.logo {
+  padding-right: 64px;
+  display: block;
+  margin: 0 auto;
+}
+
+.search_btn {
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 100%;
+  margin-right: 64px;
+  &:hover,
+  &:focus {
+    color: var(--color-accent);
+  }
+}
+</style>
